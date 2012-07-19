1153e47da3fcf7b4bd3e6c627f7bd674d0660b60
Jason Whitehorn <jason.whitehorn@gmail.com>
2012-07-18 23:16:37 -0500

    cleaned up the syntax a little

--- app.rb
+++ app.rb
@@ -11,10 +11,10 @@ get '/tiles/:zoom_level/:column/:row' do
   row = max_tile - params[:row].to_i
   
   image = @@mbtile.get_tile({ :column => column, :zoom_level => zoom_level, :row => row })
-  content_type(@@mbtile.tile_format)
+  content_type @@mbtile.tile_format
   if image.nil?
-    status(404)
-    image = File.read("public/missing.#{@@mbtile.tile_format}")
+    status 404
+    image = File.read "public/missing.#{@@mbtile.tile_format}"
   end
   
   image
